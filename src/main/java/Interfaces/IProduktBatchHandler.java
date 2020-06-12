b31585006c1abfeb7438b1308c94b1d2fbed5be7
Sulaiman Kasas <s195462@student.dtu.dk>
2020-06-12 14:34:51 +0200

    har lavet interfaces klasser

--- src/main/java/Interfaces/IProduktBatchHandler.java
+++ src/main/java/Interfaces/IProduktBatchHandler.java
@@ -1,4 +1,15 @@
 package Interfaces;
 
+import DTO.ProduktBatchDTO;
+
+import java.util.List;
+
 public interface IProduktBatchHandler {
+
+    ProduktBatchDTO getProduktBatch(int produktbatch) throws DALException;
+    List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+    void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+    void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+    void deleteProduktBatch(int produktbatch) throws DALException;
+
 }
