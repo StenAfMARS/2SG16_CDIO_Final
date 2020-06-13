9e11b83684a97522063a66fc2c270b60428dbfa2
AmerBiro <55982187+AmerBiro@users.noreply.github.com>
2020-06-13 11:47:06 +0200

    DTO klasserne er blevet udfyldt

--- src/main/java/DTO/RaavareDTO.java
+++ src/main/java/DTO/RaavareDTO.java
@@ -1,6 +1,13 @@
 package DTO;
 
 public class RaavareDTO {
-    public RaavareDTO() {
+    public RaavareDTO(int raavareId, String raavareNavn, String leverandoer) {
+        this.raavareId = raavareId;
+        this.raavareNavn = raavareNavn;
+        this.leverandoer = leverandoer;
     }
+    int raavareId;
+    String raavareNavn;
+    String leverandoer;
+
 }
--- src/main/java/DTO/UserDTO.java
+++ src/main/java/DTO/UserDTO.java
@@ -1,6 +1,21 @@
 package DTO;
 
+import java.util.List;
+
 public class UserDTO {
-    public UserDTO() {
+    public UserDTO(int oprId, String oprNavn, String ini, List<String> roller) {
+        this.oprId = oprId;
+        this.oprNavn = oprNavn;
+        this.ini = ini;
+        this.roller = this.roller;
     }
+
+    int oprId;
+    /** Bruger navn (opr_navn) min. 2 max. 20 karakterer */
+    String oprNavn;
+    /** Bruger initialer min. 2 max. 4 karakterer */
+    String ini;
+    /** Bruger cpr-nr 10 karakterer */
+    List<String> roller;
+
 }
