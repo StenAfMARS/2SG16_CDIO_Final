9feba561759a89226487ff1276df92e4c6c7e5eb
mathias <mathias.baerentzen@gmail.com>
2020-06-20 12:27:15 +0200

    cookies created

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -1,14 +1,16 @@
+$(document).ready(function() {
+    alert(getCookie(IdCard))
+});
 
-// A $( document ).ready() block.
 $("#logOnForm").submit(function(e) {
     e.preventDefault();
 });
 function switchPage(page){
-    $("body").load(page);
+    $("main").load(page);
 }
 
 
-function login() {
+function login(keyCardValue) {
     var data = $('#logOnForm').serializeJSON();
     $.ajax({
         url: 'rest/login',
@@ -18,6 +20,9 @@ function login() {
         data: data,
         success: function (data) {
             alert(JSON.stringify(data));
+            var userID =document.getElementById("UserID").value;
+            var value = "userID ="+userID+";keyCardValue="+keyCardValue;
+            createCookie(IdCard, value, 30)
             switchPage('Views/userAdminstrationPage.html')
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -26,4 +31,30 @@ function login() {
             alert(errorThrown);
         }
     });
+}
+function createCookie(name, value, minits) {
+    var expires;
+    if (minits) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minits * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
 }
