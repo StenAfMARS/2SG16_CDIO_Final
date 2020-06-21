fc4eb824d5516ab4e330dbd4294cc75d65ed8f6e
Peter Skaarup <peterskaarup@gmail.com>
2020-06-21 11:45:24 +0200

    Added receptKomponent to ReceptDTO

--- src/main/java/DTO/ReceptDTO.java
+++ src/main/java/DTO/ReceptDTO.java
@@ -4,11 +4,18 @@ public class ReceptDTO {
 
     int receptId;
     String receptNavn;
+    ReceptKomponentDTO receptKomponent;
 
-    public ReceptDTO(int receptId, String receptNavn) {
+    public ReceptDTO(int receptId, String receptNavn, ReceptKomponentDTO components) {
         this.receptId = receptId;
         this.receptNavn = receptNavn;
+        this.receptKomponent = components;
     }
+
+    public int getReceptID() {return receptId;}
+    public String getReceptNavn() {return receptNavn;}
+    public ReceptKomponentDTO getReceptKomponent() {return receptKomponent;}
+
 
 
 }
