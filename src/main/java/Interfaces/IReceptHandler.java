cbb240561bdba5ce39bdd4d0863de2fae84e0744
Peter Skaarup <peterskaarup@gmail.com>
2020-06-21 13:32:01 +0200

    Added ReceptKomponent to IReceptHandler

--- src/main/java/Interfaces/IReceptHandler.java
+++ src/main/java/Interfaces/IReceptHandler.java
@@ -1,14 +1,21 @@
 package Interfaces;
 
 import DTO.ReceptDTO;
+import DTO.ReceptKomponentDTO;
 import Exceptions.DALException;
 
 import java.util.List;
 
 public interface IReceptHandler {
+
     ReceptDTO getRecept(int receptId) throws DALException;
     List<ReceptDTO> getReceptList() throws DALException;
     void createRecept(ReceptDTO recept) throws DALException;
     void updateRecept(ReceptDTO recept) throws DALException;
+
+    ReceptKomponentDTO getReceptKomponent(int receptKomponentID) throws DALException;
+    List<ReceptKomponentDTO> getReceptKomponentList() throws DALException;
+    void createReceptKomponent(ReceptKomponentDTO receptkomponent) throws DALException;
+    void updateReceptKomponent(ReceptKomponentDTO receptKomponent) throws DALException;
 
 }
