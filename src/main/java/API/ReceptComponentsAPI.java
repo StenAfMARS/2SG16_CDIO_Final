7aeeb0b25289fada354b7581700590d947e3dc06
Peter Skaarup <peterskaarup@gmail.com>
2020-06-23 21:28:06 +0200

    Added ReceptComponentsAPI.java

---
+++ src/main/java/API/ReceptComponentsAPI.java
@@ -0,0 +1,14 @@
+package API;
+
+import DTO.ReceptKomponentDTO;
+import Exceptions.DALException;
+import Handlers.ReceptHandler;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public class ReceptComponentsAPI {
+
+}
