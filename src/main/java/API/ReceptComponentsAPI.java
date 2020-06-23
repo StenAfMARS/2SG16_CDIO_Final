d7f3cda88e7086bc4b1d3cf79a422e7178699d1e
Peter Skaarup <peterskaarup@gmail.com>
2020-06-23 22:01:34 +0200

    Updated ReceptComponentsAPI.java

--- src/main/java/API/ReceptComponentsAPI.java
+++ src/main/java/API/ReceptComponentsAPI.java
@@ -1,7 +1,9 @@
 package API;
 
+import DTO.ProduktBatchKompDTO;
 import DTO.ReceptKomponentDTO;
 import Exceptions.DALException;
+import Handlers.ProduktBatchHandler;
 import Handlers.ReceptHandler;
 
 import javax.ws.rs.*;
@@ -9,6 +11,28 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Path("ReceptComponents")
 public class ReceptComponentsAPI {
+    private static ReceptHandler receptComponents;
+    public static ReceptHandler receptComponents(){
+        if (receptComponents == null)
+            receptComponents = new ReceptHandler();
+        return receptComponents;
+    }
+
+    @Path("{id : [0-9]+}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public static List<ReceptKomponentDTO> get(@PathParam("id") int ReceptID){
+        return receptComponents().getReceptKomponentList();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public static List<ReceptKomponentDTO> getList()
+    {
+        return receptComponents().getReceptKomponentList();
+    }
+
 
 }
