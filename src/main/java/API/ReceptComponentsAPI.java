a66d0a4e3d1049ca774ae2ae24c0fc715df81648
Peter Skaarup <peterskaarup@gmail.com>
2020-06-23 22:05:28 +0200

    Updated ReceptComponentsAPI.java

--- src/main/java/API/ReceptComponentsAPI.java
+++ src/main/java/API/ReceptComponentsAPI.java
@@ -34,5 +34,32 @@ public class ReceptComponentsAPI {
         return receptComponents().getReceptKomponentList();
     }
 
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public static Response update(ReceptKomponentDTO receptKomponents){
+        try {
+            receptComponents().updateReceptKomponent(receptKomponents);
+        } catch (DALException e) {
+            e.printStackTrace();
+
+            return Response.status(418).build();
+        }
+
+        return Response.status(201).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public static Response create(ReceptKomponentDTO receptKomponents){
+        try {
+            receptComponents().createReceptKomponent(receptKomponents);
+        } catch (DALException e){
+            e.printStackTrace();
+
+            return Response.status(418).build();
+        }
+
+        return Response.status(201).build();
+    }
 
 }
